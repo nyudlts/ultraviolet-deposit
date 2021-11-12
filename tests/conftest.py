d1e1623233a42fa6f3f7881d0ab8bbe927134a9d
katepechekhonova <ekate@nyu.edu>
2021-11-12 06:42:21 -0500

    add javascript to test

--- tests/conftest.py
+++ tests/conftest.py
@@ -20,6 +20,11 @@ from flask_babelex import Babel
 
 from ultraviolet_deposit.ext import UltravioletDeposit
 from ultraviolet_deposit.views import blueprint
+from invenio_assets import InvenioAssets
+import jinja2
+from invenio_search import InvenioSearch
+from invenio_db import InvenioDB
+
 
 
 
@@ -32,22 +37,33 @@ def celery_config():
     return {}
 
 
+
 @pytest.fixture(scope='module')
 def create_app(instance_path):
     """Application factory fixture."""
     def factory(**config):
         app = Flask('testapp', instance_path=instance_path)
         app.config.update(**config)
+
+        # Adding the temporal path to jinja engine.
+        app.jinja_loader = jinja2.ChoiceLoader([
+            jinja2.FileSystemLoader('.'),
+            app.jinja_loader
+        ])
+        app.config['WEBPACKEXT_MANIFEST_PATH'] = ''
         Babel(app)
         UltravioletDeposit(app)
+        InvenioAssets(app)
+        InvenioSearch(app)
+        InvenioDB(app)
         app.register_blueprint(blueprint)
         app.template_folder = 'tests'
+        app.static_folder = ''
         @app.route("/")
         def deposit_form():
             """Deposit form fixture."""
             return render_template(
-                "index.html",
+                "tests/index.html",
             )
         return app
     return factory
-
