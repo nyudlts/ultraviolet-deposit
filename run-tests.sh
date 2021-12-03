134260f582940269d9e99a83f4229b7b60ca6ce3
katepechekhonova <ekate@nyu.edu>
2021-12-03 02:19:21 -0500

    Added requirements checks

--- run-tests.sh
+++ run-tests.sh
@@ -9,6 +9,52 @@
 
 # Usage:
 #   env DB=postgresql12 SEARCH=elasticsearch7 CACHE=redis MQ=rabbitmq ./run-tests.sh
+#check that pipenv is installed
+if ! command -v pipenv &> /dev/null
+then
+    echo "pipenv could not be found"
+    exit 1
+fi
+
+#check if pytest and fixtures are installed
+if ! command -v pipenv run pytest --version&> /dev/null
+then
+    echo "pytest is not installed"
+    exit 1
+fi
+
+#make sure pytest.ini is present
+if ! command docker -v  &> /dev/null 
+then
+    echo "docker is not istalled. Please install it"
+    exit 1
+fi
+
+#make sure pytest.ini is present
+if ! test -f pytest.ini
+then
+    echo "pytest.ini does not exists. Are you in the right directory ?"
+    exit 1
+fi
+
+#check that requirements are met if we are doing E2E testing
+if [ "${E2E}" == 'yes' ]
+then
+   #check that chromedriver is installed
+   if ! command -v  chromedriver &> /dev/null
+   then
+      echo "Chromedriver could not be found. You can not run e2e testing"
+      exit 1
+   fi
+   #check that selenium client is installed
+   if ! pipenv graph | grep 'selenium'
+   then
+      echo "Selenium client is not installed. You can not run e2e testing"
+      exit 1
+   fi
+fi
+
+
 
 # Quit on errors
 set -o errexit
